142f776874207afe7d7f408f7eba4b5b4a0b66a8
irfanbaig7 <ibaig9356@gmail.com>
2025-06-27 15:29:54 +0530

    add user and video model

--- 04_youtubePRJ/src/util/apiError.js
+++ 04_youtubePRJ/src/util/apiError.js
@@ -7,6 +7,7 @@ class apiError extends Error {
   ) {
     super(message);
     this.statusCode = statusCode;
+    this.data = null;
     this.message = message;
     this.success = false;
     this.errors = errors;
