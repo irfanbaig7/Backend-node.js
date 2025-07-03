c90fd684986f0cfc5469693dcfd956bee9b92cf0
irfanbaig7 <ibaig9356@gmail.com>
2025-07-03 23:12:44 +0530

    fix all register controller bugs

--- 04_youtubePRJ/src/controllers/user.controller.js
+++ 04_youtubePRJ/src/controllers/user.controller.js
@@ -1,95 +1,88 @@
-import { asyncHandler } from "../util/asyncHandler.js"
-import { User } from "../models/user.model.js"
-import { uploadOnCloudinary } from "../util/cloudinary.js"
-import { apiError } from "../util/apiError.js"
-import { apiResponse } from "../util/apiResponce.js"
+import { asyncHandler } from "../util/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../util/cloudinary.js";
+import { apiError } from "../util/apiError.js";
+import { apiResponse } from "../util/apiResponce.js";
 
-const registerUser = asyncHandler( async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  // get user details from frontend (postman)
 
-    // get user details from frontend (postman) 
+  const { username, fullname, email, password } = req.body;
 
-    const {username, fullname, email, password} = req.body
-    console.log("Email : " , email);
-    
+  // validation like can user pass empty anything?
 
+  if (
+    [fullname, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new apiError(400, "All field are required");
+  }
 
-    // validation like can user pass empty anything?
+  // check if user already exist ? how can we check?
 
-    if (
-        [fullname, username, email, password].some((field) => field?.trim() === "")
-    ) {
-        throw new apiError(400, "All field are required")
-    }
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
 
-
-    // check if user already exist ? how can we check?
-
-    const existedUser = await User.findOne({
-        $or : [
-            { username },
-            { email },
-        ]
-    })
-
-    if (existedUser) {
-        throw new apiError(409, "User with Email or Username already Exists")
-    }
+  if (existedUser) {
+    throw new apiError(409, "User with Email or Username already Exists");
+  }
 
 
-    // check images, check avatar specailly
+  // check images, check avatar specailly
 
-    const avatarLocalPath =  req.files?.avatar[0]?.path;
-    const coverImageLocalPath =  req.files?.coverImage[0]?.path;
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const coverImageLocalPath =  req.files?.coverImage[0]?.path;  // this will be working but hence if w'll be not provude theme in that case this will be not handel error's.
 
-    if (!avatarLocalPath) {
-        throw new apiError(400, "Avatar file is required")
-    }
+  // checking for coverimage
+  let coverimageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.lenght > 0
+  ) {
+    coverimageLocalPath = req.files.coverImage[0].path;
+  }
 
+  // checking for AvatarImage
+  if (!avatarLocalPath) {
+    throw new apiError(400, "Avatar file is required");
+  }
 
-    // uloade theme to cloudinary , we already have for that one utility
+  // uloade theme to cloudinary , we already have for that one utility
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage  = await uploadOnCloudinary(coverImageLocalPath)
-     
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverimageLocalPath);
 
-    if (!avatar) {
-        throw new apiError(400, "Avatar file is required!")
-    }
+  if (!avatar) {
+    throw new apiError(400, "Avatar file is required!");
+  }
 
+  // create a user Object - ceration call entry in DB
 
-    // create a user Object - ceration call entry in DB
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
 
-    const user = await User.create({
-        fullname,
-        avatar: avatar.url,
-        coverImage: coverImage?.url || "",
-        email,
-        password,
-        username: username.toLowerCase(),
-    })
+  // remove password and refreshToken field from response
 
-    
-    // remove password and refreshToken field from response
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
-    const createdUser = await User.findById(user._id).select(
-        "-password -refreshToken"
-    )
+  if (!createdUser) {
+    throw new apiError(500, "Something went Wrong while registering the User");
+  }
 
-    if (!createdUser) {
-        throw new apiError(500, "Something went Wrong while registering the User")
-    }
+  // return Response
 
+  return res
+    .status(201)
+    .json(new apiResponse(200, createdUser, "User Registered Successfully"));
+});
 
-
-    // return Response
-
-    return res.status(201).json(
-        new apiResponse(200, createdUser, "User Registered Successfully")
-    )
-
-
- 
-})
-
-
-export {registerUser}  
+export { registerUser };
